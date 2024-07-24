6a96a444bc4fd5b5ffceb70bc1f1e53a1293595b
Safalya Kumbhare <safalyakumbhare@gmail.com>
2024-07-24 11:30:44 +0530

    w3 wchool clone

---
+++ js/w3school.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".topic").click(function(){
+        $(".htmlsub").slideToggle(500);
+    });
+
+    $(".csstopic").click(function(){
+        $(".csssub").slideToggle(500);
+    })
+    $(".jqtopic").click(function(){
+        $(".jqsub").slideToggle(500);
+    })
+});
+
